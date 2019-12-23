10994aa276b8f9d5ac338fa1eeba322b2eb3c892
Ming Li <mili@csm-wl-dhcp-201-219.mines.edu>
2019-12-22 18:24:37 -0700

    Change lintex.ins

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|pdf)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|dtx|ins|tex|pdf)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
@@ -11,7 +11,7 @@ latex lintex.ins
 pdflatex lintex.dtx
 
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|pdf|cls)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|cls)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
