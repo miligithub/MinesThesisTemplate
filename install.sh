b3de913d941e447834eee751e3b87f4a8cf6f7b8
Ming Li <mili@csm-wl-dhcp-200-156.mines.edu>
2020-01-10 16:12:21 -0700

    Finish references

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty|bib)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
@@ -11,7 +11,7 @@ pdflatex lintex.dtx
 pdflatex lintex.dtx
 
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty|cls)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty|bib|cls)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
