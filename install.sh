7b85e5d3f391112a7af600a415d286b94023be3b
Ming Li <mili@csm-wl-dhcp-200-156.mines.edu>
2020-01-11 16:50:54 -0700

    Modify .sh for .md file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+echo "This bash file will remove all files except sh|md|dtx|ins|tex|pdf|sty|bib"
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty|bib)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|md|dtx|ins|tex|pdf|sty|bib)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
@@ -11,7 +12,7 @@ pdflatex lintex.dtx
 pdflatex lintex.dtx
 
 for file in *; do
-    if [[ ! $file =~ (sh|dtx|ins|tex|pdf|sty|bib|cls)$ &&  -f $file ]]; then 
+    if [[ ! $file =~ (sh|md|dtx|ins|tex|pdf|sty|bib|cls)$ &&  -f $file ]]; then 
         # echo "remove $file"
         rm $file
     fi
